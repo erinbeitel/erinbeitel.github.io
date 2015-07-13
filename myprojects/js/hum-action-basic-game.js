474795a6ebc93ae94bcf31e2709ffed43244b2ad
Erin Beitel <erin.beitel@gmail.com>
2015-07-13 03:28:12 -0500

    update

--- myprojects/js/hum-action-basic-game.js
+++ myprojects/js/hum-action-basic-game.js
@@ -19,37 +19,47 @@ var player = {
 move: function() {
   while (player.win !== true) {
 
-    // if (player.posX === rebel.posX && player.posY === rebel.posY){
-    //   player.food -= 10; 
-    //   document.getElementById("player-food").innerHTML = player.food;
-    //   console.log("You encountered the rebel. You lost some food.!");
-    //   console.log(player);
-    // };
+    if (rebel.posX-100 <= player.posX && player.posX <= rebel.posX+100 && rebel.posY-100 <= player.posY && player.posY <= rebel.posY+100){
+      player.food -= 10; 
+      setTimeout(function(){document.getElementById("player-food").innerHTML = player.food;},800); 
+      setTimeout(function(){document.getElementById("player-stat1").innerHTML = "You encountered the rebel. You lost some food!"},800); 
+      // document.getElementById("player-stat1").innerHTML = "You encountered the rebel. You lost some food!";
+      setTimeout(function(){document.getElementById("player-stat2").innerHTML = player.stat()},800); 
+      // document.getElementById("player-stat2").innerHTML = player.stat();
+    };
 
     if(player.posX >= camp.posX && player.posY >= camp.posY){
       if (player.food >= 50){
         player.win = true;
-        console.log("You reached the camp with enough food, you win!!");
+        setTimeout(function(){document.getElementById("player-stat3").innerHTML = "You reached the camp with enough food, you win!!"},1500); 
+        // document.getElementById("player-stat3").innerHTML = "You reached the camp with enough food, you win!!";
       }
       else {
         player.win = false;
-        console.log("You reached the camp but you don't have enough food, you lose!");
+        setTimeout(function(){document.getElementById("player-stat3").innerHTML = "You reached the camp but you don't have enough food, you lose!"},1500); 
+        // document.getElementById("player-stat3").innerHTML = "You reached the camp but you don't have enough food, you lose!";
       }
     }
     else {
       console.log("Keep going!");
-      player.posX += Math.floor((Math.random()*100)+1);
-      document.getElementById("player-locationX").innerHTML = player.posX;
-      player.posY += Math.floor((Math.random()*100)+1);
-      document.getElementById("player-locationY").innerHTML = player.posY;
-      player.win = false;
+      player.posX += Math.floor((Math.random()*10)+1);
+      // document.getElementById("player-locationX").innerHTML = player.posX;
+      setTimeout(function(){document.getElementById("player-locationX").innerHTML = player.posX;},800); 
+      player.posY += Math.floor((Math.random()*10)+1);
+      // document.getElementById("player-locationY").innerHTML = player.posY;
+      setTimeout(function(){document.getElementById("player-locationY").innerHTML = player.posY},800); 
+      rebel.posX += Math.floor((Math.random()*10)+1);
+      rebel.posY += Math.floor((Math.random()*10)+1);
+      // document.getElementById("player-stat2").innerHTML = player.stat();
+      setTimeout(function(){document.getElementById("player-stat2").innerHTML = player.stat()},800); 
     };
   };
 
 },
 
-status: function() {
-  console.log("Your position is " + player.posX + "," + player.posY + " you have" + player.food + "food left");
+stat: function() {
+  return "Your position is (" + player.posX + "," + player.posY + ") and you have " + player.food + " lbs. of food left.";
+  document.getElementById("player-stat").innerHTML = player.stat();
 }
 };
 
