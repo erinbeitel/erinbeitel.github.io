793a58fba346e4eabb86682dea028e101f82c5d8
Erin Beitel <erin.beitel@gmail.com>
2015-07-13 12:47:49 -0500

    some updates

--- myprojects/js/hum-action-basic-game.js
+++ myprojects/js/hum-action-basic-game.js
@@ -1,13 +1,14 @@
 var camp = {
 	posX: 400,
 	posY: 400,
-	population: 200,
-	food_needed: 50
+	population: 200
+// food_needed is population / 4
 };
 
 var rebel = {
 	posX: 100,
-	posY: 100
+	posY: 100,
+  food: 0
 };
 
 var player = {
@@ -18,9 +19,10 @@ var player = {
 
 move: function() {
   while (player.win !== true) {
-
+//After every move, check if the player is near the rebels or the camp
     if (rebel.posX-100 <= player.posX && player.posX <= rebel.posX+100 && rebel.posY-100 <= player.posY && player.posY <= rebel.posY+100){
-      player.food -= 10; 
+      player.food -= 10;
+      rebel.food += 10;
       setTimeout(function(){document.getElementById("player-food").innerHTML = player.food;},800); 
       setTimeout(function(){document.getElementById("player-stat1").innerHTML = "You encountered the rebel. You lost some food!"},800); 
       // document.getElementById("player-stat1").innerHTML = "You encountered the rebel. You lost some food!";
@@ -29,7 +31,7 @@ move: function() {
     };
 
     if(player.posX >= camp.posX && player.posY >= camp.posY){
-      if (player.food >= 50){
+      if (player.food >= camp.population/4){
         player.win = true;
         setTimeout(function(){document.getElementById("player-stat3").innerHTML = "You reached the camp with enough food, you win!!"},1500); 
         // document.getElementById("player-stat3").innerHTML = "You reached the camp with enough food, you win!!";
@@ -63,40 +65,10 @@ stat: function() {
 }
 };
 
-//After every move, check if the player is near the rebels or the camp
-
-// while (player.win !== true) {
-
-// if (player.posX === rebel.posX && player.posY === rebel.posY){
-//   player.food -= 10; 
-//   document.getElementById("player-food").innerHTML = player.food;
-//   console.log("You encountered the rebel. You lost some food.!");
-//   console.log(player);
-// };
-
-// if(player.posX >= camp.posX && player.posY >= camp.posY){
-//   if (player.food >= 50){
-//     player.win = true;
-//     console.log("You reached the camp with enough food, you win!!");
-//   }
-//   else {
-//     player.win = false;
-//     console.log("You reached the camp but you don't have enough food, you lose!");
-//   }
-// }
-// else {
-// 	console.log("Keep going!");
-//   player.posX += Math.floor((Math.random()*100)+1);
-//   document.getElementById("player-locationX").innerHTML = player.posX;
-//   player.posY += Math.floor((Math.random()*100)+1);
-//   document.getElementById("player-locationY").innerHTML = player.posY;
-// };
 
 
-// };
 
 
-// range = document.createRange();
-// range.selectNode(document.getElementsByTagName("div").item(0));
-// bool = range.isPointInRange(document.getElementsByTagName("p").item(0),1);
+
+
 
